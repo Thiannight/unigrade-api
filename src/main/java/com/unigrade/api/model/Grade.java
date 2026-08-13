7fb496bd057c19cc2ba21e5efacac942d4d30d9b
Thiannight <thianntatiana@gmail.com>
2026-08-13 19:54:23 +0300

    chore: format

--- src/main/java/com/unigrade/api/model/Grade.java
+++ src/main/java/com/unigrade/api/model/Grade.java
@@ -4,9 +4,4 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public record Grade(
-    UUID id,
-    Float score,
-    OffsetDateTime gradeDate,
-    String reason,
-    String studentId,
-    UUID examId) {}
+    UUID id, Float score, OffsetDateTime gradeDate, String reason, String studentId, UUID examId) {}
